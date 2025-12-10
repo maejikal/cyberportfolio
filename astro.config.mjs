5a5d5a30a5be34f5de3ed5703575a250ce696400
maejikal <meylinggga@gmail.com>
2025-12-11 02:29:12 +0800

    deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
+  site: "https://maejikal.github.io/cyberportfolio/",
+})
